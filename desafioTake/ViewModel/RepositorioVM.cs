9ebcdc9e18477b50aab620d86fa3841abf8068e7
Marina Brito Gomes <marina.brito@take.net>
2022-11-22 11:58:32 -0300

    Adicionar arquivos de projeto.

---
+++ desafioTake/ViewModel/RepositorioVM.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace RepositoriosTakeAPI.ViewModels
+{
+    public class RepositorioVM
+    {
+        [JsonProperty("full_name")]
+        public string NomeRepositorio { get; set; }
+
+        [JsonProperty("description")]
+        public string Descricao { get; set; }
+
+        [JsonProperty("created_at")]
+        public DateTime DataCriacao { get; set; }
+
+        [JsonProperty("language")]
+        public string Linguagem { get; set; }
+
+        [JsonProperty("owner")]
+        public ImagemVM Imagem { get; set; }
+    }
+
+    public class ImagemVM
+    {
+        [JsonProperty("avatar_url")]
+        public string Avatar { get; set; }
+    }
+}
